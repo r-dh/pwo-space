20cda5eff4fd02df5a4b54d79acee080130dabcb
r_dh <remy.dheygere@howest.be>
2020-11-12 12:17:50 +0100

    Add mock audio: default reply to recognized occurrence of "Hallo"

--- speech.js
+++ speech.js
@@ -42,11 +42,38 @@ btn.addEventListener("click", () => {
 recognition.onresult = function (event) {
 	const last = event.results.length - 1;
 	const text = event.results[last][0].transcript;
-	console.log(text);
+	console.log("Recognised: " + text);
 	updateBottomStatus(text);
+	//Delete input after three seconds
+	setTimeout( function() { updateBottomStatus(); }, 3000);
+	answer(text);
+	//send to SPACE backend
 }
 
 recognition.onend = function (event) {
 	updateInfoStatus(false)
 	btn.style.display = ""
+}
+
+function answer(input) {
+	//play audio if input contains "hello"
+	if (input.includes("Hallo") || input.includes("hallo")) {
+		//play audio
+		//experiment with howlerjs?
+		var audio = new Audio('./audio/int_voorstellen.wav');
+		audio.play();
+	} else {
+		console.log("No hallo found")
+	}
+/*	$.ajax({
+		url: '/process',
+		data: {"user_input" : $.trim(userText)},
+		type: 'POST',
+		success: function(response) {
+			//
+		},
+		error: function(error){
+			console.log(error);
+		}
+	});*/
 }
