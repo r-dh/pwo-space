b5f74f10e16ef077b6b2427eb6c3e329d6dc6600
r_dh <remy.dheygere@howest.be>
2020-11-24 16:00:31 +0100

    Update deprecated import

--- chatbot/data/data_to_classifier.py
+++ chatbot/data/data_to_classifier.py
@@ -12,7 +12,7 @@ from sklearn.feature_extraction.text import TfidfVectorizer
 from sklearn.model_selection import train_test_split
 from sklearn.ensemble import RandomForestClassifier
 from sklearn.metrics import classification_report, confusion_matrix, accuracy_score
-from sklearn.externals import joblib
+import joblib
 
 from database import Database
 
